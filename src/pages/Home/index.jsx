e6c050bd3ae8adc5a0d338039934cd16d0f27ea4
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2023-12-21 11:58:55 +0000

    :sparkles:	feat: add fetch api data, isLoading and error message

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,60 @@
+import { useEffect, useState } from 'react';
 import Banner from '../../components/Banner';
 import Card from '../../components/Card';
 import Title from '../../components/Title';
 import styles from './Home.module.css';
-import videos from '../../json/db.json';
 
 const Home = () => {
+  const [videos, setVideos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setIsError(false);
+    fetch('https://my-json-server.typicode.com/gilbertouk/cine-tag/videos')
+      .then((response) => response.json())
+      .then((data) => {
+        setVideos(data);
+        setIsError(false);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+        setIsError(true);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <Banner image={'home'} />
+        <Title>
+          <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+        </Title>
+        <section>
+          <p className={styles.loading}>Loading...</p>
+        </section>
+      </>
+    );
+  }
+
+  if (isError) {
+    return (
+      <>
+        <Banner image={'home'} />
+        <section>
+          <Title>
+            <h2 className={styles.error}>
+              Ops! Ocorreu algum erro aqui, tente recarregar a pagina!
+            </h2>
+          </Title>
+        </section>
+      </>
+    );
+  }
+
   return (
     <>
       <Banner image={'home'} />
--- src/pages/Player/index.jsx
+++ src/pages/Player/index.jsx
@@ -2,17 +2,62 @@ import styles from './Player.module.css';
 import Banner from '../../components/Banner';
 import Title from '../../components/Title';
 import { useParams } from 'react-router-dom';
-import videos from '../../json/db.json';
 import { useEffect, useState } from 'react';
 import NotFound from '../NotFound';
 
 const Player = () => {
   const params = useParams();
   const [video, setVideo] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    setVideo(videos.find((video) => video.id === +params.id));
+    setIsLoading(true);
+    setIsError(false);
+    fetch(
+      `https://my-json-server.typicode.com/gilbertouk/cine-tag/videos/${params.id}`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setVideo(data);
+        setIsError(false);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+        setIsError(true);
+      });
   }, [params]);
+
+  if (isLoading) {
+    return (
+      <>
+        <Banner image="player" />
+        <Title>
+          <h1>Player</h1>
+        </Title>
+        <section>
+          <p className={styles.loading}>Loading...</p>
+        </section>
+      </>
+    );
+  }
+
+  if (isError) {
+    return (
+      <>
+        <Banner image="player" />
+        <section>
+          <Title>
+            <h2 className={styles.error}>
+              Ops! Ocorreu algum erro aqui, tente recarregar a pagina!
+            </h2>
+          </Title>
+        </section>
+      </>
+    );
+  }
 
   if (!video) {
     return <NotFound />;
